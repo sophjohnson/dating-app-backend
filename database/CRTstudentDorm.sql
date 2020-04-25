5e2e7a85ff0b4e79b7030bd393abb4c86b2d0e12
Sophie Johnson <sjohns37@nd.edu>
2020-04-25 21:20:00 +0000

    Delete unused tables

--- database/CRTstudentDorm.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE StudentDorm;
-
-CREATE TABLE StudentDorm(
-    netid varchar2(32),
-    dorm varchar2(32),
-    CONSTRAINT pkStudentDorm PRIMARY KEY (netid, dorm),
-    CONSTRAINT fkDormNetid FOREIGN KEY (netid) REFERENCES Student(netid),
-    CONSTRAINT fkDorm FOREIGN KEY (dorm) REFERENCES Dorm(dorm)
-);
